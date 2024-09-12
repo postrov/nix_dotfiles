cdf324cfd4cd5c5aae3c5d8fe8f000ade73ac9c0
Pawel Ostrowski <pasza@pasza.org>
2024-09-12 11:48:12 +0200

    Disabled capslock remapping

--- system/nixos/configuration.nix
+++ system/nixos/configuration.nix
@@ -89,7 +89,7 @@
         ids = ["*"];
         settings = {
           main = {
-            capslock = "overload(meta, esc)";
+            # capslock = "overload(meta, esc)";
           };
         };
       };
